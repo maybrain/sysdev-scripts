53875ab3740956a6dead5d1def6dd5b8cb40beda
Ignacio Fernández <may@localhost.localdomain>
2014-07-25 11:00:31 +0200

    aniadido saltos de linea mac y lectura de rutas correcta

--- sanitize_index_js/script.py
+++ sanitize_index_js/script.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
 import datetime
 
 def ahora():
-	return str(datetime.datetime.now())
+        return str(datetime.datetime.now())
 
-searchline = '["style", "-webkit-transform-origin", '
+#searchline = '["style", "-webkit-transform-origin", '
+searchline = '", "-webkit-transform-origin", '
 terceravariable = '-webkit-transform-origin'
 legitline = 'transform-origin'
 
@@ -18,30 +18,37 @@ forigen = open(origen)
 lorigen = forigen.readlines()
 forigen.close()
 
+
 for line in lorigen:
-	change = False;
-	fline = open(line)
-	lines = fline.readlines()
-	fline.close()
-	i = 0
-	for l in lines:		
-		#print str(i) + " " +  str(l.find(searchline)) + ": " + str(l)
-		if l.find(searchline)!=-1:
-		 	print ahora() + " encuentra!"
-		 	lines.insert(i+1, lines[i].replace(terceravariable,legitline) )
-		 	change = True
-		i = i + 1
-	#escribe lines en line
-	if change:
-		fline = open(line,"w")
-		for l in lines:
-			fline.write(l)
-		fline.close()
+        change = False;
+        mac = False
+        fline = open(line.strip("\n"))
+        lines = fline.readlines()
+        #print line + ' ' + str(len(lines))
+        #lines = aux.splitlines()
+        fline.close()
+        if len(lines)==1:
+                #print "MAC " + line
+                aux = str(lines[0]).split("\r")
+                lines = aux
+                mac = True
+        i = 0
+        for l in lines:
+                #print str(i) + " " +  str(l.find(searchline)) + ": " + str(l)
+                if l.find(searchline)!=-1:
+                        #print ahora() + " " + str(line)+" "+str(l) +" encuentra!"
+                        lines.insert(i+1, lines[i].replace(terceravariable,legitline) )
+                        change = True
+                i = i + 1
+        #escribe lines en line
+        if change:
+                fline = open(line.strip("\n"),"w")
+                for l in lines:
+                        if mac:
+                                fline.write(l+'\n')
+                        else:
+                                fline.write(l)
+                        #print "kk"
+                fline.close()
 
 print ahora() + " end!"
-
-# for i in `cat /home/ubuntu/rutas_edge_archivos.txt`
-# do 
-# echo $i
-# grep -n "\-webkit\-transform\-origin" $i 
-# done > /home/ubuntu/archivitoconwebkits.txt  &
